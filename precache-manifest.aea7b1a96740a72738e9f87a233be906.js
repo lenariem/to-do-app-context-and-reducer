e67b217d9a9f75e44c8aaee37f3eb347f01a21dc
LenaIT <olena.denysova@gmail.com>
2020-09-29 15:17:39 +0200

    Updates

---
+++ precache-manifest.aea7b1a96740a72738e9f87a233be906.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48dcb2d71293429f896701390eaf7635",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c0d5d8234a4e92f706f2",
+    "url": "./static/css/main.5d67a9a3.chunk.css"
+  },
+  {
+    "revision": "327e91ef261293f72285",
+    "url": "./static/js/2.9ec6e10b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.9ec6e10b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0d5d8234a4e92f706f2",
+    "url": "./static/js/main.8acdc7db.chunk.js"
+  },
+  {
+    "revision": "46c523c0d6d656549b3f",
+    "url": "./static/js/runtime-main.84878c42.js"
+  }
+]);

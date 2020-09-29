3753e4437450a7bcbcd6f7e9ea0050ea7c943ca5
LenaIT <olena.denysova@gmail.com>
2020-09-29 15:24:39 +0200

    console.log deleted

--- src/components/Reducer.js
+++ src/components/Reducer.js
@@ -1,11 +1,8 @@
 export const Reducer =(state ,action)=> {
-    console.log(state,action);
     switch (action.type) {
         case "addItem": 
-        console.log(action);
             return [...state,{id:state.length,text:action.payload,done:false}]
         case "updateItem": 
-        console.log(action);
             return state.map(item=>item.id===action.payload? {...item,done:!item.done} : item)
         case "deleteItem": 
             return state.filter(item=> item.id!==action.payload )
